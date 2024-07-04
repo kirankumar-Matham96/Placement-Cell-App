194e4e2652a014b4b55c5267ed80214a26d74c8c
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-07-04 17:23:32 +0530

    feat: added welcome message on base API url

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use("/api/placement-cell/results", auth, resultRoutes);
 app.get("/", (req, res, next) => {
   res.status(200).send({ message: "Welcome to Placement Cell" });
 });
-app.use("*", unknownPathHandlerMiddleware);
+app.use("/*", unknownPathHandlerMiddleware);
 
 // app level error handling middleware
 app.use(errorHandlingMiddleware);
